4de3195b519d365fabd2bfd0cae302950f8d08b7
Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>
2024-09-12 12:23:59 -0500

    mow

--- kobold/arch/IArchitecture.hpp
+++ kobold/arch/IArchitecture.hpp
@@ -1,14 +1,19 @@
 #pragma once
 #include "../general/Common.hpp"
 
-typedef enum {
-    DISABLE_INTERRUPTS,
-    ENABLE_INTERRUPTS,
-    YIELD_UNTIL_INTERRUPT,
-} IArch_IntAction;
+namespace Kobold {
+    namespace Architecture {
+        typedef enum {
+            DISABLE_INTERRUPTS,
+            ENABLE_INTERRUPTS,
+            YIELD_UNTIL_INTERRUPT,
+        } IntAction;
 
-class IKoboldArchitecture {
-public:
-    virtual void InterruptControl(IArch_IntAction action);
-    virtual void Log(const char* s, size_t l);
-};
+        void InterruptControl(IntAction action);
+        void Log(const char* s, size_t l);
+        void Initialize();
+
+        int GetHartID();
+        
+    }
+}
