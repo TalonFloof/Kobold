0d1900cbc8d77c56311ddf4e007bebda4c587ea0
Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>
2024-09-13 20:17:34 -0500

    finally got it in high memory omg
    Signed-off-by: Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>

--- kobold/arch/riscv64/Startup.s
+++ kobold/arch/riscv64/Startup.s
@@ -1,24 +1,29 @@
-.section .text
-
+.section .text.boot
 .global _start
-_start:
-  la      sp, __rv64_init_stack_top
 
-  la      t0, rv64_boot_page_table
+.option push
+_start:
+  li t2, 0xffff800000000000
+  la sp, __rv64_init_stack_top-0xffff800000000000
+  add sp, sp, t2
+
+  la t0, rv64_boot_page_table-0xffff800000000000
   srli    t0, t0, 12
   li      t1, 9 << 60
   or      t0, t0, t1
   csrw    satp, t0
   sfence.vma
 
-  call KernelInitialize
+  la t0, KernelInitialize-0xffff800000000000
+  add t0, t0, t2
+  or ra, zero, zero
+  jr t0
+.option pop
 
-.section .bss
 .align 12
 __rv64_init_stack_bottom:
-   .space 8192
+   .zero 4096
 __rv64_init_stack_top:
-.section .data
 .align 12 # Make sure the entries are aligned
 rv64_boot_page_table:
   .quad (0x0 << 37) | 0xcf # VRWXAD
