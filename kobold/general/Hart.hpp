e14df938a45603be699f8589d4549dc9dcc1ca1d
Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>
2024-09-22 11:03:28 -0500

    Update Hart.hpp

--- kobold/general/Hart.hpp
+++ kobold/general/Hart.hpp
@@ -3,5 +3,13 @@
 
 namespace Kobold {
     struct Hart {
+        usize tempReg1;
+        usize tempReg2;
+        usize activeUserStack;
+        usize activeSyscallStack;
+        usize activeTrapStack;
+        Thread* activeThread;
+        void* kstackTop;
+        usize reserved;
     };
 }
