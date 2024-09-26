97e443f1cfcede75628fc42d299ba1818898046f
Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>
2024-09-25 21:48:17 -0500

    gfdgfd
    Signed-off-by: Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>

--- kobold/general/Userspace/Scheduler.hpp
+++ kobold/general/Userspace/Scheduler.hpp
@@ -6,9 +6,9 @@ namespace Kobold::Userspace {
     struct ScheduleQueue {
         Thread* head;
         Thread* tail;
-        Kobold::Lock lock = {0,false,false};
+        Kobold::Sync::Lock lock = {0,false,false};
 
         void AddToQueue(Thread* t);
         Thread* PullFromQueue();
-    }
+    };
 }
