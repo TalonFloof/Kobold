ab2fa284ceedc41e7372b752ababe46fae5f281b
Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>
2024-10-23 14:18:57 -0500

    mrr

--- kobold/kernel/pfn.zig
+++ kobold/kernel/pfn.zig
@@ -1,13 +1,20 @@
 // TODO: PFN Support
 const std = @import("std");
 
-var pfnStart: usize = 0;
-var pfnEntries: usize = 0;
+var pfn: ?[]PFNEntry = null;
+var pfnBaseAddr: usize = 0;
+
+pub enum PFNEntryType = enum(u8) {
+    gpp = 0, // General Purpose Page
+    reserved = 1, // Memory that will never be reclaimed
+    pageTable = 2, // Memory Reserved for a branch of a page table
+    pageDir = 3, // Memory Reserved for the trunk of a page table
+}
 
 pub const PFNEntry = packed struct {
     ref: usize,
     pte: u56,
-    pfnType: u8,
+    pfnType: PFNEntryType,
 };
 
 pub fn init() void {}
