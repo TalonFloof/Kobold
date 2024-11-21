b633af9bffaa42408ee2c6961f69dee48479e101
Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>
2024-11-21 14:18:50 -0600

    mrr

--- kobold/kernel/vfs.zig
+++ kobold/kernel/vfs.zig
@@ -3,14 +3,17 @@ const std = @import("std");
 pub const VNode = extern struct {
     name: [256]u8 = [_]u8{0} ** 256.
 
-    unreferenced: ?*fn (*VNode) callconv(.C) c_int = null,
-    open: ?*fn(*VNode) callconv(.C) c_int = null,
-    close: ?*fn(*VNode) callconv(.C) c_int = null,
-    readDir: ?*fn(*VNode, u32, *DirEntry) callconv(.C) c_int = null,
-    findDir: ?*fn(*VNode, [*c]const u8, *?*VNode) callconv(.C) c_int = null,
-    truncate: ?*fn(*VNode, usize) callconv(.C) c_int = null,
-    create: ?*fn(*VNode, [*c]const u8, usize) callconv(.C) c_int = null,
-    unlink: ?*fn(*VNode, [*c]const u8) callconv(.C) c_int = null,
-    rename: ?*fn(*VNode, [*c]const u8, *VNode, [*c]const u8) callconv(.C) c_int = null,
-    ioctl: 
+    unreferenced: ?*fn (*VNode) callconv(.C) c_long = null,
+    open: ?*fn(*VNode) callconv(.C) c_long = null,
+    close: ?*fn(*VNode) callconv(.C) c_long = null,
+    readDir: ?*fn(*VNode, u32, *DirEntry) callconv(.C) c_long = null,
+    findDir: ?*fn(*VNode, [*c]const u8, *?*VNode) callconv(.C) c_long = null,
+    truncate: ?*fn(*VNode, usize) callconv(.C) c_long = null,
+    create: ?*fn(*VNode, [*c]const u8, usize) callconv(.C) c_long = null,
+    unlink: ?*fn(*VNode, [*c]const u8) callconv(.C) c_long = null,
+    rename: ?*fn(*VNode, [*c]const u8, *VNode, [*c]const u8) callconv(.C) c_long = null,
+    ioctl: ?*fn(*VNode, c_ulong, ?*anyopaque) callconv(.C) c_long = null,
+
+    read: ?*fn(*VNode, usize, *anyopaque, usize) callconv(.C) c_long = null,
+    write: ?*fn(*VNode, usize, *anyopaque, usize) callconv(.C) c_long = null,
 };
