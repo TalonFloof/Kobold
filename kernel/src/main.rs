7fe60af9d34264bc61dacabb97174d90a0053605
Talon Kettuso <105325988+TalonFloof@users.noreply.github.com>
2025-06-30 11:22:57 -0500

    stuff

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+pub mod HAL;
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
